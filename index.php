791f651e2f75d96618005afb88935b7d71a0780c
Daniel Henrique <Redoaxe@users.noreply.github.com>
2025-04-19 01:08:35 -0300

    Update index.php
    UPDATED values to follow the change in banco.sql

--- index.php
+++ index.php
@@ -8,22 +8,12 @@
 <?php
 ini_set("display_errors", 1);
 header('Content-Type: text/html; charset=iso-8859-1');
-
-
-
 echo 'Versao Atual do PHP: ' . phpversion() . '<br>';
-
 $servername = "54.234.153.24";
 $username = "root";
 $password = "Senha123";
 $database = "meubanco";
-
-// Criar conexão
-
-
 $link = new mysqli($servername, $username, $password, $database);
-
-/* check connection */
 if (mysqli_connect_errno()) {
     printf("Connect failed: %s\n", mysqli_connect_error());
     exit();
@@ -34,7 +24,7 @@ $valor_rand2 = strtoupper(substr(bin2hex(random_bytes(4)), 1));
 $host_name = gethostname();
 
 
-$query = "INSERT INTO dados (AlunoID, Nome, Sobrenome, Endereco, Cidade, Host) VALUES ('$valor_rand1' , '$valor_rand2', '$valor_rand2', '$valor_rand2', '$valor_rand2','$host_name')";
+$query = "INSERT INTO mercado (ClienteID, Nome, Sobrenome, NoProdutos, ValorDaCompra, Host) VALUES ('$valor_rand1' , '$valor_rand2', '$valor_rand2', '$valor_rand2', '$valor_rand2','$host_name')";
 
 
 if ($link->query($query) === TRUE) {
